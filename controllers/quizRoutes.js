21009517bde9d4c66ed322d59d4384dd014afb54
hollandjb <hollandjb@vcu.edu>
2019-08-25 14:35:36 -0400

    front end mostly done

--- controllers/quizRoutes.js
+++ controllers/quizRoutes.js
@@ -1,47 +1,49 @@
 const express = require("express"),
-router = express.Router();
+  router = express.Router();
 var db = require("../models");
 
 let posts = [];
 
-router.get("/api", function(req, res) {
+router.get("/api", function (req, res) {
   console.log('statusCode:', res.statusCode);
   res.send(res.body)
 
 });
 
-router.get("/api/posts", function(req, res) {
+router.get("/api/posts", function (req, res) {
   console.log('statusCode:', res.statusCode);
   db.User.findAll({})
-  .then(function(dbPost) {
-    res.json(dbPost);
-  });
+    .then(function (dbPost) {
+      res.json(dbPost);
+    });
 
 });
 
-router.post("/api/posts", function(req, res) {
+router.post("/api/posts", function (req, res) {
   console.log('statusCode:', req.statusCode);
   console.log(req.body)
-  posts.push({ score: req.body.totalScore })
+  posts.push({
+    score: req.body.totalScore
+  })
   //res.send('Successfully posted to database')
   console.log(req.body);
   db.User.create({
-    id: req.body.id,
-    email:  null,
-  userPassword:  null,
-  scoreCJ:  null,
-  scoreEcon:  null, 
-  scoreHC:  null,
-  scoreImm:  null,
-  scoreEdu:  null, 
-  scoreEnv:  null,
-  totalScore:  req.body.totalScore,
-  party:  null,
-  preferredCandidate: null, 
-  createdAt: null,
-  updatedAt: null
-})
-    .then(function(dbPost) {
+      id: req.body.id,
+      email: null,
+      userPassword: null,
+      scoreCJ: null,
+      scoreEcon: null,
+      scoreHC: null,
+      scoreImm: null,
+      scoreEdu: null,
+      scoreEnv: null,
+      totalScore: req.body.totalScore,
+      party: null,
+      preferredCandidate: null,
+      createdAt: null,
+      updatedAt: null
+    })
+    .then(function (dbPost) {
       res.json(dbPost);
     });
 
@@ -65,38 +67,14 @@ router.get("/quiz1/:quizNum", (req, res) => {
   }
 })
 */
-router.get("/quiz1", (req, res) => {
+router.get("/quiz", (req, res) => {
   console.log("quiz1route")
   res.render("quizOne");
 })
 
-router.post("/quiz1", (req, res) => {
+router.post("/quiz", (req, res) => {
   console.log("quiz1route")
   res.render("quizOne");
-})
-
-router.get("/quiz2", (req, res) => {
-  res.render("quizTwo")
-})
-
-router.get("/quiz3", (req, res) => {
-  res.render("quizThree")
-})
-
-router.get("/quiz4", (req, res) => {
-  res.render("quizFour")
-})
- 
-router.get("/quiz5", (req, res) => {
-  res.render("quizFive")
-})
-
-router.get("/quiz6", (req, res) => {
-  res.render("quizSix")
-})
-
-router.get("/quiz7", (req, res) => {
-  res.render("quizSeven")
 })
 
 router.get("/results", (req, res) => {
