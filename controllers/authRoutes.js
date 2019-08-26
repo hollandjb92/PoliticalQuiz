9d386a6304f64207a1b707de84ee710f980a88f7
hollandjb <hollandjb@vcu.edu>
2019-08-26 18:36:06 -0400

    made the quiz page the home route

--- controllers/authRoutes.js
+++ controllers/authRoutes.js
@@ -2,13 +2,10 @@ const express = require("express"),
   router = express.Router();
 
 
-router.get("/register", (req, res) => {
-  res.render("register");
-});
+
 
 router.get("/login", (req, res) => {
   res.render("login")
-
 });
 
 
