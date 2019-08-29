b49e007093398d3f71de1db23ac195597b1e0dd5
hollandjb <hollandjb@vcu.edu>
2019-08-28 22:11:02 -0400

    ...

--- controllers/quizRoutes.js
+++ controllers/quizRoutes.js
@@ -21,7 +21,7 @@ router.get("/api/posts", function (req, res) {
 
 router.get("/api/candidates", function (req, res) {
   console.log('statusCode: a', res.statusCode);
-  db.candidate.findAll({
+  db.Candidate.findAll({
       attributes: ['canname', 'totalScore', 'party', 'candidateImage', "id"]
     })
     .then(function (dbPost) {
